76789a094e139d9a76a373c1cab2fc033482fea6
NikaKereselidze <71851989+NikaKereselidze@users.noreply.github.com>
2022-03-23 11:49:04 +0400

    change

--- components/Snackbar.js
+++ components/Snackbar.js
@@ -2,27 +2,29 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React, { useState, useImperativeHandle } from "react";
 
 const Snackbar = React.forwardRef((_, ref) => {
-  const [_, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const show = () => {
+  const showBar = () => {
     setShow(true);
   };
   const hide = () => {
     setShow(false);
   };
 
-  useImperativeHandle(ref, () => ({ show, hide }));
+  useImperativeHandle(ref, () => ({ showBar, hide }));
 
-  return (
-    <View>
-      <View style={styles.view}>
-        <Text style={styles.text}>No internet connection!</Text>
-        <TouchableOpacity onPress={hide}>
-          <Text style={styles.textButton}>RETRY</Text>
-        </TouchableOpacity>
+  if (show) {
+    return (
+      <View>
+        <View style={styles.view}>
+          <Text style={styles.text}>No internet connection!</Text>
+          <TouchableOpacity onPress={hide}>
+            <Text style={styles.textButton}>RETRY</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else return null;
 });
 
 const styles = StyleSheet.create({
